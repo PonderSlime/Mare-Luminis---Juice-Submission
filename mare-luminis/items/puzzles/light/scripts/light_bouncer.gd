449dd232966bf3a903250ecaa50943c3a14d99d3
PonderSlime (PC) <micah.j.edwards1@gmail.com>
2025-01-31 14:55:44 -0700

    FINALLY FIGURED OUT RAYCAST PROPERLY!!!!

--- mare-luminis/items/puzzles/light/scripts/light_bouncer.gd
+++ mare-luminis/items/puzzles/light/scripts/light_bouncer.gd
@@ -4,7 +4,7 @@ extends Node3D
 @export var light_color: Color = Color(1, 1, 0)
 @export var beam_width = 0.1
 @export var visible_beam_parent: Node3D
-@export var active = true
+@export var active = false
 
 var normal
 var point
@@ -13,27 +13,44 @@ var ray
 var beam_mesh: MeshInstance3D
 var raycast: RayCast3D
 
+var incoming_direction
+var normal_direction
+var outgoing_direction
 
 func _ready() -> void:
+	#visible_beam_parent.global_position = global_position
 	raycast = RayCast3D.new()
 	raycast.target_position = Vector3.FORWARD * max_bounce_distance
 	raycast.enabled = true
-	add_child(raycast)
+	raycast.top_level = true
+	visible_beam_parent.add_child(raycast)
 	
 	beam_mesh = MeshInstance3D.new()
 	beam_mesh.mesh = create_beam_mesh()
-	add_child(beam_mesh)
+	raycast.add_child(beam_mesh)
 	
 func _process(delta: float) -> void:
 	if active:
-		bounce_light
+		bounce_light()
+		
+	rotation_degrees.y += 0.05
 		
 func update_bounce_vars(normal_update, point_update, ray_update, is_active):
 	normal = normal_update
 	point = point_update
 	ray = ray_update
 	active = is_active
-
+	incoming_direction = point - ray.global_transform.origin
+	incoming_direction = incoming_direction.normalized() 
+	normal_direction = incoming_direction.bounce(normal) * max_bounce_distance
+	
+	var angle_of_incidence = acos(incoming_direction.dot(normal)) 
+	var angle_of_reflection = angle_of_incidence
+	var reflected_direction = incoming_direction.rotated(Vector3.UP, -2 * angle_of_reflection) 
+	reflected_direction *= max_bounce_distance 
+	raycast.global_transform.origin = point
+	raycast.target_position = normal_direction
+## Reference light_bouncer_old.gd for how to fix the first two or three variables, I forget which!
 func bounce_light():
 	raycast.force_raycast_update()
 	
@@ -42,7 +59,7 @@ func bounce_light():
 		if hit_object.has_method("receive_light"):
 			print("Light received by: ", hit_object.name)
 			hit_object.receive_light()
-		elif hit_object.has_method("bounce_light"):
+		elif hit_object.has_method("update_vars"):
 			var normal = raycast.get_collision_normal()
 			var point = raycast.get_collision_point()
 			hit_object.update_vars(normal, point, raycast, true)
@@ -53,11 +70,18 @@ func bounce_light():
 
 
 func update_beam_visual():
-	var end_pos = raycast.get_collision_point() if raycast.is_colliding() else global_transform.origin + global_transform.basis.z * max_bounce_distance
-	beam_mesh.global_transform.origin = raycast.global_transform.origin
-	beam_mesh.rotation_degrees.x = 90
-	beam_mesh.scale.y = global_transform.origin.distance_to(end_pos)
-	beam_mesh.position.z = - beam_mesh.scale.y / 2
+	var end_pos = raycast.get_collision_point() if raycast.is_colliding() else point + normal_direction
+	
+	beam_mesh.global_transform.origin = (point + end_pos) / 2
+	 
+	var direction = (end_pos - point).normalized()
+
+	var basis = Basis.looking_at(direction, Vector3.UP)
+
+	beam_mesh.global_transform.basis = basis
+	beam_mesh.rotation_degrees.x += 90 
+	var beam_length = point.distance_to(end_pos)
+	beam_mesh.scale.y = beam_length
 	
 func create_beam_mesh():
 	var mesh = CylinderMesh.new()
--- mare-luminis/items/puzzles/light/scripts/light_bouncer_old.gd
+++ mare-luminis/items/puzzles/light/scripts/light_bouncer_old.gd
@@ -1,64 +1,47 @@
 extends Node3D
 
-@export var max_bounce_distance = 20.
+@export var max_bounce_distance = 2.5
 @export var light_color: Color = Color(1, 1, 0)
 @export var beam_width = 0.1
 @export var visible_beam_parent: Node3D
 @export var active = true
 
-var normal
-var point
-var ray
-
-var beam_mesh: MeshInstance3D
-var raycast: RayCast3D
-
-
-func _ready() -> void:
-	raycast = RayCast3D.new()
-	raycast.target_position = Vector3.FORWARD * max_bounce_distance
-	raycast.enabled = true
-	add_child(raycast)
+func bounce_light(normal, point, ray):
+	var beam_mesh: MeshInstance3D
+	var incoming_direction = point - ray.global_transform.origin
+	var outgoing_direction = incoming_direction.bounce(normal) * max_bounce_distance
+	
+	var reflected_raycast = RayCast3D.new()
+	reflected_raycast.target_position = outgoing_direction
+	
+	print(point)
+	reflected_raycast.enabled = true
+	add_child(reflected_raycast)
+	
+	reflected_raycast.force_raycast_update()
+	_cast_reflected_ray(reflected_raycast)
 	
 	beam_mesh = MeshInstance3D.new()
 	beam_mesh.mesh = create_beam_mesh()
-	add_child(beam_mesh)
-	
-func _process(delta: float) -> void:
-	if active:
-		bounce_light
-		
-func update_bounce_vars(normal_update, point_update, ray_update, is_active):
-	normal = normal_update
-	point = point_update
-	ray = ray_update
-	active = is_active
-
-func bounce_light():
-	raycast.force_raycast_update()
-	
-	if raycast.is_colliding():
-		var hit_object = raycast.get_collider()
+	visible_beam_parent.add_child(beam_mesh)
+	update_beam_visual(reflected_raycast, beam_mesh)
+			
+func _cast_reflected_ray(reflected_raycast):
+	if reflected_raycast.is_colliding():
+		var hit_object = reflected_raycast.get_collider()
 		if hit_object.has_method("receive_light"):
 			print("Light received by: ", hit_object.name)
 			hit_object.receive_light()
-		elif hit_object.has_method("bounce_light"):
-			var normal = raycast.get_collision_normal()
-			var point = raycast.get_collision_point()
-			hit_object.update_vars(normal, point, raycast, true)
+		if hit_object.has_method("bounce_light"):
+			hit_object.bounce_light()
 
-		else:
-			print("colliding, no function?!")
-	update_beam_visual()
-
-
-func update_beam_visual():
+func update_beam_visual(raycast, beam_mesh):
 	var end_pos = raycast.get_collision_point() if raycast.is_colliding() else global_transform.origin + global_transform.basis.z * max_bounce_distance
 	beam_mesh.global_transform.origin = raycast.global_transform.origin
 	beam_mesh.rotation_degrees.x = 90
 	beam_mesh.scale.y = global_transform.origin.distance_to(end_pos)
 	beam_mesh.position.z = - beam_mesh.scale.y / 2
-	
+
 func create_beam_mesh():
 	var mesh = CylinderMesh.new()
 	mesh.top_radius = beam_width
